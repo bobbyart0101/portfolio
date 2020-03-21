b3a37ceff114daaa4cf42c0066aeb6883d7d18db
elena <elvira1598@gmail.com>
2020-03-21 19:18:38 +0100

    caption

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,16 @@ AOS.init({
     duration: 600,
     easing: 'ease-in-sine',
 });
+scrollToAnchor();
+
+function scrollToAnchor() {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+}
