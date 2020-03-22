97de45f3ca6b4e2d9a87434816df5f90ea0689db
elena <elvira1598@gmail.com>
2020-03-22 10:35:13 +0100

    added modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,7 @@ AOS.init({
 scrollToAnchor();
 
 function scrollToAnchor() {
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    document.querySelectorAll('.scroll-btn ').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
 
@@ -16,3 +16,33 @@ function scrollToAnchor() {
         });
     });
 }
+
+var projectDataObjects = [
+    {
+        id: 1,
+        title: 'Danzicard',
+        imgUrl: 'assets/img/projects/danzicard.gif',
+        content: 'It is a chinese learning app made with Ionic/Angular. dfsdfsdfdsfdsfdsfd'
+    },
+    {
+        id: 2,
+        title: 'Training Tracker',
+        imgUrl: 'assets/img/projects/danzicard.gif',
+        content: 'It is a training tracker app made with Ionic/Angular and Symfony Restful Api.'
+    }
+];
+$(document).ready(function () {
+    console.log("ready!");
+
+    $('#projectModal').on('show.bs.modal', function (event) {
+        var card = $(event.relatedTarget);
+        var index = card.data('index');
+        var modal = $(this);
+        var project = projectDataObjects.find(project => project.id === index);
+        if(project){
+            modal.find('.modal-title').text(project.title);
+            modal.find('.modal-body img').attr('src', project.imgUrl);
+            modal.find('.modal-footer').text(project.content);
+        }
+    })
+});
